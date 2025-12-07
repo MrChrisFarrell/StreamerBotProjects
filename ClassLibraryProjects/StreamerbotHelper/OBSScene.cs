3edc495c5b758bff81058f56e0a5b38e0bb9bac3
Chris Farrell <mrchrisfarrell@yahoo.com>
2025-12-06 23:31:18 -0500

    adding scene from other branch. Updated AI to file to be a twitch channel reward redemption

--- ClassLibraryProjects/StreamerbotHelper/OBSScene.cs
+++ ClassLibraryProjects/StreamerbotHelper/OBSScene.cs
@@ -1,4 +1,5 @@
 ﻿namespace StreamerbotHelper;
+using System.Text.Json.Serialization;
 
 using Streamer.bot.Plugin.Interface;
 using Streamer.bot.Plugin.Interface.Enums;
@@ -8,7 +9,57 @@ using Streamer.bot.Common;
 
 
 
-public class OBSScene
+/*----- Class name should match <filename>.cs -----*/
+#if EXTERNAL_EDITOR
+public class OBSScene : CPHInlineBase
+#else
+public class CPHInline
+#endif
+/*--------------------------------------------*/
 {
- 
+    public class OBSSource
+    {
+        public string sourceName {get; set;}
+        [JsonIgnore]
+        public bool? visible {get; set;}
+        public OBSSource(string name)
+        {
+            sourceName = name;
+        }
+    }
+    public class OBSScene
+    {
+        public string sceneName {get; set;}
+        [JsonIgnore]
+        public bool isCurrentScene {get; set;} = false;
+        [JsonIgnore]
+        public Dictionary<int,OBSSource>? sources {get; set;}
+        public OBSScene(string name)
+        {
+            sceneName = name;
+        }
+    }
+    public bool Execute()
+    {
+        try
+        {
+            // Example: Setting an OBS scene based on chat input
+            CPH.TryGetArg("rawInputEscaped", out string sceneName); // Get the raw input from the command
+
+            CPH.LogInfo($"sceneName: {sceneName}");
+
+            string sceneRequest = "{\"sceneName\":\"" + sceneName + "\"}";
+
+            CPH.LogInfo($"sceneRequest: {sceneRequest}");
+
+            CPH.ObsSendRaw("SetCurrentProgramScene", sceneRequest, 0);
+            
+            return true;
+        }
+        catch (Exception ex)
+        {
+            CPH.LogInfo($"Error: {ex}");
+            return false;
+        }
+    }
 }
--- ClassLibraryProjects/StreamerbotHelper/OpenAIToFile.cs
+++ ClassLibraryProjects/StreamerbotHelper/OpenAIToFile.cs
@@ -1,6 +1,8 @@
+namespace StreamerbotHelper;
+
 using System;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
@@ -8,7 +10,19 @@ using Newtonsoft.Json.Linq;
 using System.IO;
 using System.Collections.Generic;
 
+using Streamer.bot.Plugin.Interface;
+using Streamer.bot.Plugin.Interface.Enums;
+using Streamer.bot.Plugin.Interface.Model;
+using Streamer.bot.Common.Events;
+using Streamer.bot.Common;
+
+/*----- Class name should match <filename>.cs -----*/
+#if EXTERNAL_EDITOR
+public class OpenAIToFile : CPHInlineBase
+#else
 public class CPHInline
+#endif
+/*--------------------------------------------*/
 {
     public class OpenAIMessage
     {
@@ -35,11 +49,11 @@ public class CPHInline
     }
 	public bool Execute()
 	{
-        string userName = "JimBobx235";
-        string filePath = @"D:\OBS\Messages\newsflash.txt";
-        string apiKeyFilePath = @"D:\StreamerbotKeys\openai.txt";
+        CPH.TryGetArg("user", out string userName);
+        string filePath = CPH.GetGlobalVar<string>("TwitchRewardTextFile",true);
+        string apiKeyFilePath = CPH.GetGlobalVar<string>("OpenAIKey",true);
         string chatGPTModel = "gpt-5-nano";
-        int characterLimit = 500;
+        int characterLimit = CPH.GetGlobalVar<int>("OpenAIResponseCharLimit",true);
         string chatEndPoint = "https://api.openai.com/v1/chat/completions";
         string prompt = $"generate an urban legend about user {userName}. limit to {characterLimit} characters";
 		// Grabbing api key
@@ -66,6 +80,15 @@ public class CPHInline
         string messageContent = (string)aiResponse["choices"][0]["message"]["content"];
 		
 		File.WriteAllText(filePath, messageContent);
+
+        string targetScene = CPH.GetGlobalVar<string>("TwitchRewardScrollingTextScene",true);
+        string targetSource = CPH.GetGlobalVar<string>("TwitchRewardScrollingTextSource",true);
+        int visibilitySeconds = CPH.GetGlobalVar<int>("TwitchRewardScrollingSeconds",true);
+
+        CPH.ObsSetSourceVisibility(targetScene,targetSource,true);
+        Thread.Sleep(TimeSpan.FromSeconds(visibilitySeconds));
+        CPH.ObsSetSourceVisibility(targetScene,targetSource,false);
+
 		return true;
 	}
 }
